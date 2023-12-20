ba686644d1cbe4d11398efff68f9e127ec368d41
sanko <sang.koh@hotmail.com>
2023-12-20 22:29:46 +1100

    return cookie

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const http = require("http");
+const url = require("url");
 const port = process.env.PORT || 3000;
 const server = http.createServer();
 
 server.on("request", (req, res) => {
-	if (req.url === "/") {
+	const {pathname, query} = url.parse(req.url, true);
+	const cookies = parseCookie(req.headers.cookie);
+
+	if (pathname === "/") {
 		if (req.method === "GET") {
 			res.setHeader("content-type", "text/plain");
+
+			let messages = [];
+			if ("cookie" in query) {
+				messages.push("cookie?");
+				if (cookies.cookie) {
+					messages.push("you have a cookie already..");
+				} else {
+					messages.push("here is a dragon cookie!");
+					res.setHeader("set-cookie", "cookie=dragonCookie;max-age=10");
+				}
+			} else {
+				messages.push("hi!");
+			}
+
 			res.writeHead(200);
-			res.write("dragons!");
+
+			let write = messages.map(string => `dragon: ${string}`).join("\n");
+			res.write("dragons!\n\n" + write);
 			return res.end();
 		}
 	}
@@ -15,5 +35,26 @@ server.on("request", (req, res) => {
 	res.write("404 no dragons here");
 	res.end();
 });
+
+function parseCookie(cookie) {
+	if (!cookie) return {};
+
+	let strings = cookie.split(";");
+	let arrays = strings.map(string => {
+		let index = string.indexOf("=");
+		if (!~index) return [string.trim(), null];
+
+		let key = string.substring(0, index).trim();
+		let value = string.substring(index + 1).trim();
+		return [key, value];
+	});
+
+	let object = {};
+	arrays.forEach(array => {
+		object[array[0]] = array[1];
+	});
+
+	return object;
+}
 
 server.listen(port, () => console.log(`listening on ${port}`));
